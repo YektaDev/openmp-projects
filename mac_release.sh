a2d56a0ed9cc531bf6e620cdb1c065eae0958d58
Ali Khaleqi Yekta <me@yekta.dev>
2025-01-19 10:51:38 +0330

    mac_release

--- mac_release.sh
+++ mac_release.sh
@@ -1,4 +1,15 @@
-echo "Compiling the OpenMP project into the release binary for the current macOS machine, with aggressive optimizations..."
+#!/bin/bash
+
+# ANSI escape codes for colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Compiling the OpenMP project into the release binary for the current macOS machine, with aggressive optimizations...${NC}"
+
+mkdir -p build
 
 clang++ \
   -std=c++26 \
@@ -8,6 +19,13 @@ clang++ \
   -L/opt/homebrew/opt/libomp/lib \
   -I/opt/homebrew/opt/libomp/include \
   -lomp \
-  ./main.cpp -o ./main
+  -Wall -Wextra \
+  ./main.cpp -o ./build/main-release
 
-echo "Compilation was successful."
+if [[ $? -eq 0 ]]; then
+  echo -e "${GREEN}Compilation was successful.${NC}"
+  chmod +x ./build/main-release 2> /dev/null
+else
+  echo -e "${RED}Compilation failed.${NC}"
+  exit 1
+fi
